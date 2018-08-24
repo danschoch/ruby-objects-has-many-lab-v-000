c62e7cc347e7badbe3e0e6c3fa7208b0c47c17d6
Daniel Schoch <d.schoch90@gmail.com>
2018-08-24 20:11:55 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,7 @@
 class Artist
   attr_accessor :name, :songs
 
+  @@total_songs = []
   def initialize(name)
     @name = name
     @songs = []
@@ -8,12 +9,14 @@ class Artist
 
   def add_song(song)
     @songs << song
+    @@total_songs << song
     song.artist = self
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
+    @@total_songs << song
     song.artist = self
   end
 
